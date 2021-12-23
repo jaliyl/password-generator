11cf59f0dec6fff208ca659d2b75cabdc82f6c33
Jaliyl Parham <jaliylp@gmail.com>
2021-12-23 13:57:25 -0500

    added some pseudo code for future additions and comments

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,4 @@
-console.log("Hello there")
+console.log("Seeing if this still works")
+
+// create arrays of letters and special characters
+// create alert(pop-up) message or display password in text box
