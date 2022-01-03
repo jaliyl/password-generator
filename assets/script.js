c4863279a0f4f21c3baa4f31cc3c1a65ce1f9b0a
Jaliyl Parham <jaliylp@gmail.com>
2022-01-03 15:12:50 -0500

    replaced my html/css/js code with starter template

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,35 @@
-console.log("Seeing if this still works")
+// Assignment Code
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-// create arrays of letters and special characters
-// create alert(pop-up) message or display password in text box
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var specialCharacters = [ '`','~','!','@','#','$','%','^','&','*','(',')','_',
+'-','+','=','{','[','}'];
+
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword(){
+  window.alert("How many characters do you want your password to be?");
+}
+
+function passwordOptions(){
+  prompt("Do you want uppercase characters?");
+  window.alert("Do you want lowercase characters?");
+  window.alert("Do you want special characters?");
+  window.alert("Do you want numbers?");
+}
